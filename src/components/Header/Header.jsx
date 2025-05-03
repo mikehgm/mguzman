da2bd21780b1596825b9d571e9bb5d7796ae839b
MGuzman <mguzman.pc@gmail.com>
2025-05-02 19:25:37 -0700

    mguzman - adjusting routing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import useNavToggle from '../../hooks/useNavToggle';
-import { Link } from 'react-router-dom';
-
 import styles from './Header.module.css';
 
 const Header = () => {
@@ -9,13 +7,11 @@ const Header = () => {
 
   return (
     <header id="header">
-      <h1 id="logo">
-        <Link to="/mguzman">Miguel Guzm&aacute;n</Link>
-      </h1>
+      <h1 id="logo"><a href="/mguzman">Miguel Guzm&aacute;n</a></h1>
       <button id={styles.navToggle} onClick={toggleNav} aria-label="Toggle Navigation">☰</button>
       <nav id="nav">
         <ul>
-          <li><Link to="/mguzman">Home</Link></li>
+          <li><a href="/mguzman">Home</a></li>
         </ul>
       </nav>
     </header>
